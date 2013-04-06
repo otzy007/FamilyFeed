b2bfcd5f253cf4bb3c662a6831ddb2647c4d6625
Andrei Gliga <otzy_007@yahoo.com>
2013-04-06 18:34:11 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyFeed::Application.config.secret_token = 'cd7b0148ce0ef9dabcdb5fd1ca2662c17befa859bc3a294dade6d01ba98a38e96d76700a453b53570fc72dfefee0164de5ce0f92d64c5e1c3c9e2c043a2a8121'
