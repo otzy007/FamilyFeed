663f9fb1a1e4afffdf89672327bd9a3e88534a8d
Andrei Gliga <otzy_007@yahoo.com>
2013-04-15 13:25:23 +0300

    controllers for todo, map and calendar

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,13 @@
+class MapController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
+end
---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,16 @@
+class TodoController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+  
+  def destroy
+  end
+end
