e88b75a7c5c1345ca5ef6f8903ea578ff1b2a324
Andrei Gliga <otzy_007@yahoo.com>
2013-05-09 16:26:51 +0300

    foloseste doar index pentru toate afisarile

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -7,12 +7,13 @@ class MapController < ApplicationController
 
   def show
      @json = Checkin.find_by_family_id_and_id(current_user.family.id, params[:id]).to_gmaps4rails
+     render :index
   end
   def show_all
      user_id = params[:id]
      
      @json = Checkin.find_all_by_family_id_and_user_id(current_user.family.id, user_id).to_gmaps4rails
-     render :show
+     render :index
   end
   
   def create
