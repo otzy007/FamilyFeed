6f13c54816e7f29352398859f05d1544ca706dcd
Andrei Gliga <otzy_007@yahoo.com>
2013-06-06 12:53:59 +0300

    feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,9 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+    when /the new feed page/
+      new_feed_path
+
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
