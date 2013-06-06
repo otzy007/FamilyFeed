be77b71d6daee035ec265370874bc72e512e19ef
Andrei Gliga <otzy_007@yahoo.com>
2013-06-06 19:37:32 +0300

    secret token schimbat

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FamilyFeed::Application.config.secret_token = 'cd7b0148ce0ef9dabcdb5fd1ca2662c17befa859bc3a294dade6d01ba98a38e96d76700a453b53570fc72dfefee0164de5ce0f92d64c5e1c3c9e2c043a2a8121'
+FamilyFeed::Application.config.secret_token = 'cd7b0148ce0ef9dfb315643fda2662c17befa859bc3a294dade6d128698a38e96d7670aa453b53570fc72dfefee0178905ce0f92d64c5e1c35aa2c043a238121'
