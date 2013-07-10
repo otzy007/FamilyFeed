e0ad7072bda8c12814e22976eaf995489c152f96
Andrei Gliga <otzy_007@yahoo.com>
2013-07-10 18:30:28 +0300

    autentificare cont google

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
   
   belongs_to :family
   has_many :posts
@@ -15,16 +15,28 @@ class User < ActiveRecord::Base
   has_many :invites, :through => :family
   
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-  user = User.where(:provider => auth.provider, :uid => auth.uid).first
-  unless user
-    user = User.create(name:auth.extra.raw_info.name,
-                         provider:auth.provider,
-                         uid:auth.uid,
-                         email:auth.info.email,
-                         password:Devise.friendly_token[0,20]
-                         )
+   user = User.where(:provider => auth.provider, :uid => auth.uid).first
+   unless user
+      user = User.create(name:auth.extra.raw_info.name,
+			   provider:auth.provider,
+			   uid:auth.uid,
+			   email:auth.info.email,
+			   password:Devise.friendly_token[0,20]
+			   )
+   end
+   user
   end
-  user
-end
->>>>>>> autentificare fb gata
+   
+  def self.find_for_google_oauth2(access_token, signed_in_resource = nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+        user = User.create(name: data["name"],
+	    		   email: data["email"],
+	    		   password: Devise.friendly_token[0,20]
+	    		  )
+    end
+    user
+   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -215,6 +215,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :facebook, '172844886230534', '2b0497cf80a51b4664148dc0afa62dcb', :strategy_class => OmniAuth::Strategies::Facebook
+  config.omniauth :google_oauth2, "1050529137236.apps.googleusercontent.com", "-frnclPOROGz5Kpx_ASmxffO", { access_type: "offline", approval_prompt: "" }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
