52543f79b0938d1285a71733b34caaa43f9bb309
Andrei Gliga <otzy_007@yahoo.com>
2013-07-10 18:01:34 +0300

    autentificare fb gata

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,18 @@ class User < ActiveRecord::Base
   has_many :checkins, :through => :posts
   has_many :comments, :through => :posts
   has_many :invites, :through => :family
+  
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  unless user
+    user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                         )
+  end
+  user
+end
+>>>>>>> autentificare fb gata
 end
