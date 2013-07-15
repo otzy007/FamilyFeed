25c94dc1af23aa79f7c20ec2cad3cda4ec45b655
Andrei Gliga <otzy_007@yahoo.com>
2013-07-15 22:10:10 +0300

    teste pt invitatii

--- app/controllers/admin/invite_controller.rb
+++ app/controllers/admin/invite_controller.rb
@@ -7,7 +7,7 @@ class Admin::InviteController < ApplicationController
       @email = params.require(:user).require :email
       
       if Invite.find_by_email @email
-	 redirect_to :admin_index, :alert => 'User already invited'
+	 redirect_to :admin_index, :alert => 'User already invited!'
 	 return
       end
       invite = current_user.family.invites.create email: @email, user: current_user, token: Digest::MD5.hexdigest(rand.to_s)
