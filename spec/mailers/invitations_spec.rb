5e3cf0eeb17d82048a7ede04f63a9fb5c99c5b78
Andrei Gliga <otzy_007@yahoo.com>
2013-07-18 20:16:24 +0300

    mail pt invitatii

---
+++ spec/mailers/invitations_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe Invitations do
+  describe "invite" do
+    let(:mail) { Invitations.invite('me', 'my@em.ail', 'token', 'mai@il.too') }
+
+    it "renders the headers" do
+      mail.subject.should eq("my@em.ail invited you to his FamilyFeed")
+      mail.to.should eq(["mai@il.too"])
+      mail.from.should eq(["invites@familyfeed"])
+    end
+
+    it "renders the body" do
+      mail.body.encoded.should match("nvited you into his family.")
+    end
+  end
+
+end
