dbe4fd6b460d6cbf2a815399ceae4714b30b0c87
Andrei Gliga <otzy_007@yahoo.com>
2013-07-18 20:16:24 +0300

    mail pt invitatii

---
+++ app/mailers/invitations.rb
@@ -0,0 +1,14 @@
+class Invitations < ActionMailer::Base
+  default from: "invites@familyfeed"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.invitations.invite.subject
+  #
+  def invite(name, email, token, to)
+     @name = name
+     @token = token
+     mail to: to, subject: "#{email} invited you to his FamilyFeed"
+  end
+end
