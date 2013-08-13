a539cf3ca97b3e7b30ba1516ac4b72cc7538379e
Andrei Gliga <otzy_007@yahoo.com>
2013-08-13 20:44:00 +0300

    crearea comentariului la eveniment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -3,9 +3,6 @@ class CommentController < ApplicationController
    def new
       @post = params.permit([:post])[:post]
       
-      unless @post
-	 @event = params.permit([:event])[:event]
-      end
       respond_to do |format|
 	    format.js
 	    format.html
@@ -14,11 +11,16 @@ class CommentController < ApplicationController
    
    def create
       comment = params.require(:feed).require :comment
-      post = params.require :post
-      p post
+      post = params.permit([:post])[:post]
       
-      current_user.family.posts.find_by_id(post).comments.create :text => comment, :user => current_user
-      
-      redirect_to :feed_index
+      if post
+	 current_user.family.posts.find_by_id(post).comments.create :text => comment, :user => current_user
+	 redirect_to :feed_index
+      else
+	 event = params.permit([:event])[:event]
+	 current_user.family.calendars.find(event).comments.create :text => comment, :user => current_user
+	 
+	 redirect_to calendar_path(event)
+      end
    end
 end
