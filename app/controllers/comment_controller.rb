d5b1f6159027f03c452a658e41f9d90bea5d77ec
Andrei Gliga <otzy_007@yahoo.com>
2013-08-28 11:46:20 +0300

    Revert "Revert "Merge pull request #12 from otzy007/eventcomments""
    This reverts commit b07b20e160bf491367b655fa47581ba22e9c4bfc.

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -2,10 +2,10 @@ class CommentController < ApplicationController
    
    def new
       @post = params.permit([:post])[:post]
-      p @post
+      
       respond_to do |format|
-	      format.js
-	      format.html
+	    format.js
+	    format.html
       end
    end
    
@@ -18,8 +18,17 @@ class CommentController < ApplicationController
 	 return
      end
       
-      current_user.family.posts.find_by_id(post).comments.create :text => comment, :user => current_user
-      
-      redirect_to :feed_index
+      if post
+	 current_user.family.posts.find_by_id(post).comments.create :text => comment, :user => current_user
+	 redirect_to :feed_index
+      else
+	 event = params.permit([:event])[:event]
+	 comm = current_user.family.calendars.find(event).comments.create :text => comment, :user => current_user
+	 current_user.posts.create :text => "<div class='icon-reply'></div>&nbsp;#{comment} <br /><small> on the " +
+	       "<a href=#{calendar_path(event) + '#' + comm.id.to_s}>#{Calendar.find(event).title}</a> event</small>", 
+	   :family => current_user.family
+	 
+	 redirect_to calendar_path(event)
+      end
    end
 end
