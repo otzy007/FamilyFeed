7e5afdfa45073aad56c3233ad7bd428227479a9b
Andrei Gliga <otzy_007@yahoo.com>
2013-08-28 11:49:22 +0300

    Revert PR #15 "When enter hit comment/post"
    This reverts commit 7ea120308c75e2788019f2d940e77a8a29badf02.
    
    Revert "trimite la enter si pt comentarii"
    
    This reverts commit 77e9e40aa663b9d6d3f647853ff25801299673b2.
    
    Revert "comment javascripts sterse"
    
    This reverts commit 80beb2c0aaaf0c1d24c131ea0ad066fda78e3abf.
    
    Revert "[broken] nu vrea sa trimita commentul la enter :("
    
    This reverts commit 45d0d079c8447889735332c72ce9e5b85ae9f263.
    
    Revert "posteaza mesaj la enter"
    
    This reverts commit eef85e91b92cb666f5064955890acbe5ee51c18f.

--- app/assets/javascripts/comment.js
+++
@@ -1,6 +0,0 @@
-function submit_comment(event, post) {
-   if (event.keyCode == 13) {
-      jQuery('#new-c' + post + ' > form').submit();
-      return false;
-   }
-}
--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -12,12 +12,4 @@ function showPosition(position)
   jQuery('#feed_latitude').val(position.coords.latitude);
   jQuery('#feed_longitude').val(position.coords.longitude);	
 }
-
-jQuery(document).ready(function() {
-    jQuery('#feed_post').keydown(function(event) {
-        if (event.keyCode == 13) {
-            this.form.submit();
-            return false;
-         }
-    });
-});
+  
