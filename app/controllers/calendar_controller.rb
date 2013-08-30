fd18f31c84cf7f367e7117ed56aa4706c786f663
Andrei Gliga <otzy_007@yahoo.com>
2013-08-30 22:56:07 +0300

    permit in loc de require

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,8 +2,9 @@ class CalendarController < ApplicationController
   def index
      @events = current_user.family.calendars
      
-     month = params.require(:month).to_i
-     year = params.require(:year).to_i
+     pars = params.permit :month, :year
+     month = pars[:month].to_i
+     year = pars[:year].to_i
      current_user.family.users.map do |u|
 	if u.birth && u.birth.month == month
 	   @events << Calendar.create(date: Date.new(year, u.birth.month, u.birth.day), family: current_user.family, title: "#{u.name} birthday")
