054fcc77020096abc7487afee6f70dd7de4c865a
Andrei Gliga <otzy_007@yahoo.com>
2013-09-18 12:26:53 +0300

    am setat ca administratori utilizatorii andrei,bogdan@cloduients

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def can_administer?
+     if current_user && (current_user.email == 'andrei@cloudients.com' || current_user.email == 'bogdan@cloudients.com')
+	true
+     else
+	false
+     end
+  end
 end
